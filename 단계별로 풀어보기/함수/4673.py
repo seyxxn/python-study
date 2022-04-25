b3e6dabc87d2b9893e97396db6fcfd3159ee4da3
seyxxn <lucy9480@gmail.com>
2022-04-25 23:52:56 +0900

    4673 반대로출력됨....

--- 단계별로 풀어보기/함수/4673.py
+++ 단계별로 풀어보기/함수/4673.py
@@ -1,27 +1,74 @@
 # def isNotSelfNum(n):
-#     list = [[int(i) for i in range(1,n+1)]]
-#     for x in list:
+#     numList = [int(i) for i in range(1,n+1)]
+#     for x in numList:
 #         if len(str(x)) == 1:
 #             if (x + int(str(x)[0]) == x):
 #                 print(x)
-#                 return True 
+#                 # return True 
 #         elif len(str(x)) == 2:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) == i):
+#             if (x + int(str(x)[0]) + int(str(x)[1]) == x):
 #                 print(x)
-#                 return True        
+#                 # return True        
 #         elif len(str(x)) == 3:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) == i):
+#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) == x):
 #                 print(x)
-#                 return True  
+#                 # return True  
 #         elif len(str(x)) == 4:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) == i):
+#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) == x):
 #                 print(x)
-#                 return True  
+#                 # return True  
 #         elif len(str(x)) == 5:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) + int(str(x)[4]) == i):
+#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) + int(str(x)[4]) == x):
 #                 print(x)
-#                 return True
-#         return False
+#                 # return True
+#         # return False
 
 
-# isNotSelfNum(10000)
+# isNotSelfNum(100)
+
+# numList = [int(i) for i in range(1,100)]
+
+numList = [int(i) for i in range(1,101)]
+
+def plusNum(x):
+    result = 0
+    if len(str(x)) == 1:
+        result = x + int(str(x)[0])
+        return result
+    elif len(str(x)) == 2:
+        result = x + int(str(x)[0]) + int(str(x)[1])
+        return result
+    elif len(str(x)) == 3:
+        result = x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2])
+        return result
+    elif len(str(x)) == 4:
+        result = x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3])
+        return result
+    elif len(str(x)) == 5:
+        result = x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) + int(str(x)[4])
+        return result
+
+def isNotSelfNum():
+    for x in numList:
+        if len(str(x)) == 1:
+            for y in range(1,x):
+                if (plusNum(y) == x):
+                    print(x)
+        elif len(str(x)) == 2:
+            for y in range(1,x):
+                if (plusNum(y) == x):
+                    print(x)
+        elif len(str(x)) == 3:
+            for y in range(1,x):
+                if (plusNum(y) == x):
+                    print(x)
+        elif len(str(x)) == 4:
+            for y in range(1,x):
+                if (plusNum(y) == x):
+                    print(x)
+        elif len(str(x)) == 5:
+            for y in range(1,x):
+                if (plusNum(y) == x):
+                    print(x)
+
+isNotSelfNum()
