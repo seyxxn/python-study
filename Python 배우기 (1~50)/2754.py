22fa23e669a3871f6ffe27fa2e4a6200522b71fc
seyxxn <lucy9480@gmail.com>
2022-04-26 00:59:01 +0900

    2754

---
+++ Python 배우기 (1~50)/2754.py
@@ -0,0 +1,28 @@
+x = input()
+
+if x == 'A+':
+    print(4.3)
+elif x == 'A0':
+    print(4.0)
+elif x == 'A-':
+    print(3.7)
+elif x == 'B+':
+    print(3.3)
+elif x == 'B0':
+    print(3.0)
+elif x == 'B-':
+    print(2.7)
+elif x == 'C+':
+    print(2.3)
+elif x == 'C0':
+    print(2.0)
+elif x == 'C-':
+    print(1.7)
+elif x == 'D+':
+    print(1.3)
+elif x == 'D0':
+    print(1.0)
+elif x == 'D-':
+    print(0.7)
+elif x == 'F':
+    print(0.0)
