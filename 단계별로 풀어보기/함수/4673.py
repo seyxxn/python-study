568b4ef38ffbe3a5a192b0ed7eac2c0b27b88be2
seyxxn <lucy9480@gmail.com>
2022-04-26 00:26:45 +0900

    ** 4673

--- 단계별로 풀어보기/함수/4673.py
+++ 단계별로 풀어보기/함수/4673.py
@@ -1,47 +1,14 @@
-numList = [int(i) for i in range(1,10001)]
+def selfNum():
+    numList = list(range(1,10001)) # 1-10000 리스트 생성
+    notSelfNumList = [] # self
+    for x in numList :
+        for i in str(x):
+            x += int(i)
+        if x <= 10000:
+            notSelfNumList.append(x)
+    for y in set(notSelfNumList):
+        numList.remove(y)
+    for z in numList:
+        print(z)
 
-def plusNum(x):
-    result = 0
-    if len(str(x)) == 1:
-        result = x + int(str(x)[0])
-        return result
-    elif len(str(x)) == 2:
-        result = x + int(str(x)[0]) + int(str(x)[1])
-        return result
-    elif len(str(x)) == 3:
-        result = x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2])
-        return result
-    elif len(str(x)) == 4:
-        result = x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3])
-        return result
-    elif len(str(x)) == 5:
-        result = x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) + int(str(x)[4])
-        return result
-
-def isNotSelfNum(x):
-    if len(str(x)) == 1:
-        for y in range(1,x):
-            if (plusNum(y) == x):
-                return True
-    elif len(str(x)) == 2:
-        for y in range(1,x):
-            if (plusNum(y) == x):
-                return True
-    elif len(str(x)) == 3:
-        for y in range(1,x):
-            if (plusNum(y) == x):
-                return True
-    elif len(str(x)) == 4:
-        for y in range(1,x):
-            if (plusNum(y) == x):
-                return True
-    elif len(str(x)) == 5:
-        for y in range(1,x):
-            if (plusNum(y) == x):
-                return True
-
-for x in numList:
-    if isNotSelfNum(x):
-        pass
-    else:
-        print(x)
+selfNum()
