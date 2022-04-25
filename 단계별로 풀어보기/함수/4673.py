ab6ad69de894e765e51cbdfff4710e4ab2105942
seyxxn <lucy9480@gmail.com>
2022-04-26 00:05:10 +0900

    4673 시간초과

--- 단계별로 풀어보기/함수/4673.py
+++ 단계별로 풀어보기/함수/4673.py
@@ -1,34 +1,4 @@
-# def isNotSelfNum(n):
-#     numList = [int(i) for i in range(1,n+1)]
-#     for x in numList:
-#         if len(str(x)) == 1:
-#             if (x + int(str(x)[0]) == x):
-#                 print(x)
-#                 # return True 
-#         elif len(str(x)) == 2:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) == x):
-#                 print(x)
-#                 # return True        
-#         elif len(str(x)) == 3:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) == x):
-#                 print(x)
-#                 # return True  
-#         elif len(str(x)) == 4:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) == x):
-#                 print(x)
-#                 # return True  
-#         elif len(str(x)) == 5:
-#             if (x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) + int(str(x)[4]) == x):
-#                 print(x)
-#                 # return True
-#         # return False
-
-
-# isNotSelfNum(100)
-
-# numList = [int(i) for i in range(1,100)]
-
-numList = [int(i) for i in range(1,101)]
+numList = [int(i) for i in range(1,10001)]
 
 def plusNum(x):
     result = 0
@@ -48,27 +18,30 @@ def plusNum(x):
         result = x + int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]) + int(str(x)[3]) + int(str(x)[4])
         return result
 
-def isNotSelfNum():
-    for x in numList:
-        if len(str(x)) == 1:
-            for y in range(1,x):
-                if (plusNum(y) == x):
-                    print(x)
-        elif len(str(x)) == 2:
-            for y in range(1,x):
-                if (plusNum(y) == x):
-                    print(x)
-        elif len(str(x)) == 3:
-            for y in range(1,x):
-                if (plusNum(y) == x):
-                    print(x)
-        elif len(str(x)) == 4:
-            for y in range(1,x):
-                if (plusNum(y) == x):
-                    print(x)
-        elif len(str(x)) == 5:
-            for y in range(1,x):
-                if (plusNum(y) == x):
-                    print(x)
+def isNotSelfNum(x):
+    if len(str(x)) == 1:
+        for y in range(1,x):
+            if (plusNum(y) == x):
+                return True
+    elif len(str(x)) == 2:
+        for y in range(1,x):
+            if (plusNum(y) == x):
+                return True
+    elif len(str(x)) == 3:
+        for y in range(1,x):
+            if (plusNum(y) == x):
+                return True
+    elif len(str(x)) == 4:
+        for y in range(1,x):
+            if (plusNum(y) == x):
+                return True
+    elif len(str(x)) == 5:
+        for y in range(1,x):
+            if (plusNum(y) == x):
+                return True
 
-isNotSelfNum()
+for x in numList:
+    if isNotSelfNum(x):
+        pass
+    else:
+        print(x)
