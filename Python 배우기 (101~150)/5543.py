ec32aff5ad82af8db26254c20a4664b2d58d7c19
seyxxn <lucy9480@gmail.com>
2022-05-18 17:35:26 +0900

    5543

---
+++ Python 배우기 (101~150)/5543.py
@@ -0,0 +1,12 @@
+sang = int(input())
+joong = int(input())
+ha = int(input())
+
+burger = [sang, joong, ha]
+
+coke = int(input())
+cider = int(input())
+
+beverage = [coke, cider]
+
+print(min(burger)+min(beverage)-50)
