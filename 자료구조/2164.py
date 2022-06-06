e09bbbe75dd01325a74a956cb40666f447e9b06e
seyxxn <lucy9480@gmail.com>
2022-06-06 19:07:59 +0900

    2164 - queue 사용

---
+++ 자료구조/2164.py
@@ -0,0 +1,15 @@
+# queue를 사용해야할것같음
+
+from collections import deque
+x = int(input())
+queue = deque([])
+for i in range(1, x+1):
+    queue.append(i)
+
+while len(queue) != 1:
+    queue.popleft()
+    x = queue.popleft()
+    queue.append(x)
+
+print(queue[0])
+
